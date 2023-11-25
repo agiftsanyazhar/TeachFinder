a2958a1a1197e04196562e450a0b14b497ff580b
Agiftsany Azhar <agiftsanyazhar@gmail.com>
2023-11-25 15:06:22 +0700

    update

--- resources/views/mails/send-email-verify.blade.php
+++ resources/views/mails/send-email-verify.blade.php
@@ -27,28 +27,28 @@
 </head>
 
 <body style="margin:0; padding:0;">
-  <table role="presentation" style="width:100%; border-collapse:collapse; border:0; border-spacing:0; ">
+  <table role="presentation" style="width:100%; border-collapse:collapse; border:0; border-spacing:0;">
     <tr>
-      <td style="padding:0; background:#fff; ">
-        <table role="presentation" style="width:100%; border-collapse:collapse; border:0; border-spacing:0; ">
+      <td style="padding:0;">
+        <table role="presentation" style="width:100%; border-collapse:collapse; border:0; border-spacing:0;">
           <tr>
-            <td style="padding:20px 40px; ">
-              <p style="margin:0; font-size:18px; font-family:Inter,sans-serif; color:#212529; font-style:normal; font-weight:600; ">Halo {{ $name }},</p>
-              <p style="margin:0; font-size:16px; font-family:Inter,sans-serif; color:#212529; font-style:normal; font-weight:500; ">{{ $email }}</p>
+            <td style="padding:20px 40px;">
+              <p style="margin:0; font-size:18px; font-family:Inter,sans-serif; color:#212529; font-style:normal; font-weight:600;">Halo {{ $name }},</p>
+              <p style="margin:0; font-size:16px; font-family:Inter,sans-serif; color:#212529; font-style:normal; font-weight:500;">{{ $email }}</p>
 
-              <p style="margin:30px 0 0 0; font-size:16px; font-family:Inter,sans-serif; color:#212529; font-style:normal; font-weight:500; ">
+              <p style="margin:30px 0 0 0; font-size:16px; font-family:Inter,sans-serif; color:#212529; font-style:normal; font-weight:500;">
                 Silakan verifikasi akun Anda terlebih dahulu.
               </p>
 
-              <table role="presentation" style="margin:20px 0; width:100%; border-collapse:collapse; border:0; border-spacing:0; ">
+              <table role="presentation" style="margin:20px 0; width:100%; border-collapse:collapse; border:0; border-spacing:0;">
                 <tr>
-                  <td align="center" style="padding:0; ">
-                    <table role="presentation" style="width:auto; border-collapse:collapse; border:0; border-spacing:0; ">
+                  <td align="center" style="padding:0;">
+                    <table role="presentation" style="width:auto; border-collapse:collapse; border:0; border-spacing:0;">
                       <tr>
-                        <td align="center" style="background:#1B8BAE; border-radius:8px; padding:0; ">
+                        <td align="center" style="background:#1B8BAE; border-radius:8px; padding:0;">
                           <form action="{{ route('send-email-verify', $token) }}" method="post">
                             <input type="hidden" name="email" value="{{ $email }}">
-                            <button type="submit" style="background:#1B8BAE; border:none; font-size:14px; margin:18px 12px; line-height:0; font-family:Ubuntu,sans-serif; color:#ffffff; font-style:normal; font-weight:500; ">
+                            <button type="submit" style="background:#1B8BAE; border:none; font-size:14px; margin:18px 12px; line-height:0; font-family:Ubuntu,sans-serif; color:#ffffff; font-style:normal; font-weight:500;">
                               Verifikasi
                             </button>
                           </form>
@@ -59,24 +59,24 @@
                 </tr>
               </table>
 
-              <p style="margin:30px 0 0 0; font-size:16px; font-family:Inter,sans-serif; color:#212529; font-style:bold; font-weight:500; ">
+              <p style="margin:30px 0 0 0; font-size:16px; font-family:Inter,sans-serif; color:#212529; font-style:bold; font-weight:500;">
                 Verifikasi ini akan kedalawuarsa dalam 60 menit.
               </p>
-              <p style="margin:50px 0 0 0; font-size:16px; font-family:Inter,sans-serif; color:#212529; font-style:bold; font-weight:500; ">Salam,<br>TeachFinder</p>
+              <p style="margin:50px 0 0 0; font-size:16px; font-family:Inter,sans-serif; color:#212529; font-style:bold; font-weight:500;">Salam,<br>TeachFinder</p>
             </td>
           </tr>
         </table>
       </td>
     </tr>
     <tr>
-      <td style="padding:0; ">
-        <table role="presentation" style="width:100%; border-collapse:collapse; border:0; border-spacing:0; ">
+      <td style="padding:0;">
+        <table role="presentation" style="width:100%; border-collapse:collapse; border:0; border-spacing:0;">
           <tr>
-            <td style="padding:0; ">
-              <table align="center" role="presentation" style="border-collapse:collapse; border:0; border-spacing:0; ">
+            <td style="padding:0;">
+              <table align="center" role="presentation" style="border-collapse:collapse; border:0; border-spacing:0;">
                 <tr>
-                  <td align="center" style="padding:0; width:auto; ">
-                    <p style="margin:100px 0 0 0;  font-size:14px; font-family:Inter,sans-serif; color:#012970; font-style:normal; ">&copy;  Copyright <b>{{ date('Y') }} TeachFinder</b>. All Rights Reserved</p>
+                  <td align="center" style="padding:0; width:auto;">
+                    <p style="margin:100px 0 0 0;  font-size:14px; font-family:Inter,sans-serif; color:#012970; font-style:normal;">&copy;  Copyright <b>{{ date('Y') }} TeachFinder</b>. All Rights Reserved</p>
                   </td>
                 </tr>
               </table>
